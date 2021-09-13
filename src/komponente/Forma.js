28fa469e81847b9a355b3abc14aee5ea5e69033c
ilija12354 <ilijakoske@gmail.com>
2021-09-13 03:26:50 +0200

    Uradjena validacija i pozadina za formu

--- src/komponente/Forma.js
+++ src/komponente/Forma.js
@@ -6,37 +6,54 @@ import {useForm} from 'react-hook-form';
 function Forma(props) {
   
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    alert("Uspesno ste se prijavili!");
+  };
 
 
   return (
     <>
+    <div className='video-container'>
+    <video src='/videos/video-2.mp4' autoPlay loop muted />
     <div class="forma-container">
     <form class="form" onSubmit={handleSubmit(onSubmit)}>
     <div class="title">Dobro dosli</div>
     <div class="subtitle">Prijavite se</div>
     <div class="input-container ic1">
-    <input id="firstname" class="input" type="text" placeholder=" "{...register("ime",{required: true})} />
+    <input id="firstname" class="input" type="text" placeholder=" "{...register("ime",{required: true,maxLength: 20})} />
       {errors.ime &&  <div class="upozorenje"><span>Obavezno polje!</span></div>}
       <div class="cut"></div>
       <label for="firstname" class="placeholder">Ime</label>
       </div>
-      <div class="input-container ic2">
-        <input id="prezime" class="input" type="text" placeholder=" " {...register("prezime", { required: true })} />
+      <div class="input-container ic1">
+        <input id="prezime" class="input" type="text" placeholder=" " {...register("prezime", { required: true,maxLength: 20 })} />
       {errors.prezime && <div class="upozorenje"><span>Obavezno polje!</span></div>}
         <div class="cut"></div>
         <label for="prezime" class="placeholder">Prezime</label>
       </div>
       
-      <div class="input-container ic2">
-        <input id="email" class="input" type="text" placeholder=" " {...register("prezime", { required: true })} />
-      {errors.prezime && <div class="upozorenje"><span>Obavezno polje!</span></div>}
+      <div class="input-container ic1">
+        <input id="email" class="input" type="text" placeholder=" " {...register("email", { required: true,pattern:/^\S+@\S+$/i })} />
+      {errors.email && <div class="upozorenje"><span>Unesite email u formatu nesto@nesto</span></div>}
         <div class="cut cut-short"></div>
         <label for="email" class="placeholder">Email</label>
+      </div>
+      <div class="input-container ic1">
+      <select class="destinacija" id="destinacija"{...register("Destinacija", { required: true })}>
+        <option >Izaberite destinaciju</option>
+        <option value="Grdica">Grdica</option>
+        <option value="Kraljevo">Kraljevo</option>
+        <option value="Beograd">Beograd</option>
+        <option value="Pancevo">Pancevo</option>
+        <option value="Vrnjacka banja">Vrnjacka banja</option>
+      </select>
+      {errors.Destinacija && <div class="upozorenje"><span>Izaberite destinaciju</span></div>}
       </div>
       <button type="text" class="submit">Posaljite prijavu</button>
       
     </form>
+    </div>
     </div>
  </>
   );
